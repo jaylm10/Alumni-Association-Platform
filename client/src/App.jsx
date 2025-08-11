290c5fa2163594bf14826400caed8c8656e746f9
Jay <jay.parmar.lm10@gmail.com>
2025-08-11 18:24:01 +0530

    Fix context issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
-import UserTypeselection from './pages/UserTypeselection'
-import Register from './pages/Register'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import JobBoard from './pages/JobBoard'
-import EventsPage from './pages/EventsPage'
+import { useState } from "react";
+import UserTypeselection from "./pages/UserTypeselection";
+import Register from "./pages/Register";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import JobBoard from "./pages/JobBoard";
+import EventsPage from "./pages/EventsPage";
 
 
 function App() {
-
   return (
     <>
-    <Routes>
-     <Route path="/" element={<Home/>}/>
-     <Route path="/userTypeSelection" element={<UserTypeselection/>}/>
-     <Route path="/register" element={<Register/>}/>
-     <Route path="/login" element={<Login/>}/>
-     <Route path="/Jobs" element={<JobBoard/>}/>
-     <Route path="/Events" element={<EventsPage/>}/>
-    </Routes>
     
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/userTypeSelection" element={<UserTypeselection />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Jobs" element={<JobBoard />} />
+          <Route path="/Events" element={<EventsPage />} />
+        </Routes>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
